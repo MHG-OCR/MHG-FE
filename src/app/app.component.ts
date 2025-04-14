e65bb8d4c06d2b6d35899a865a2d84dff9bf87a0
tgaud <156192044+tgaud@users.noreply.github.com>
2025-04-14 13:27:06 +0200

    init momentum favicon, header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppHeaderComponent } from '@lib/header/header.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.less',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppHeaderComponent],
 })
 export class AppComponent {
   public title = 'my-app';
---
+++ src/lib/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-component',
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.less',
+  standalone: true,
+})
+export class AppHeaderComponent {
+}
